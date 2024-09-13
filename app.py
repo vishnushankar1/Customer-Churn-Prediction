dbf42ba3e34dca70b1546d296e555d4ed1e91d3b
vishnu shankar <121851474+vishnushankar1@users.noreply.github.com>
2024-09-13 21:13:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ input_data = pd.DataFrame({
 })
 
 # One-hot encode 'Geography'
-geo_encoded = onehot_encoder_geo.transform([[geography]])
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
 # Combine one-hot encoded columns with input data
